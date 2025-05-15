aa8545a15bab7efc405191bf43b96a8b3fa722ad
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-05-15 16:53:21 -0300

    starting to practice sqlite

---
+++ sqlite3_study/main.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+from pathlib import Path
+
+ROOT_FILE = Path(__name__).parent
+DB_FOLDER_NAME = 'sqlite3_study'  # Nome da pasta onde o banco de dados será criado
+DB_FOLDER = ROOT_FILE / DB_FOLDER_NAME
+DB_NAME = 'db_sqlite3'
+DB_FILE = DB_FOLDER / DB_NAME
+TABLE_NAME = 'customers'
+
+DB_FOLDER.mkdir(parents=True, exist_ok=True)
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+
+cursor.close()
+connection.close()
