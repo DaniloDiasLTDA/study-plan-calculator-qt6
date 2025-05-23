f0c9a88d888a1c0fc52d0c87456e900acaa70f26
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-05-23 11:22:20 -0300

    feat: Delete freelancers.db and add announcement teste

--- peewee_study/main.py
+++ peewee_study/main.py
@@ -7,31 +7,44 @@ db.connect()
 
 db.create_tables([Users, Announcement])
 
-# user = Users.create(
-#         name="Danilo Dias",
-#         email="dias@gmail.com",
-#         password="000111"
-#     )
-# user = Users.create(
-#         name="Luis Souza",
-#         email="souzaluis@hotmail.com",
-#         password="000111"
-#     )
-# user = Users.create(
-#         name="Agatha Dantas",
-#         email="datas@outlook.com",
-#         password="000111"
-#     )
+try:
+    user = Users.create(
+            name="Danilo Dias",
+            email="dias@gmail.com",
+            password="000111"
+        )
+except IntegrityError:
+    print({'error': 'Este usuario já está cadastrado.'})
 
 
-# try:
-#     user = Users.create(
-#         name="ProgamadorPython",
-#         email="devpython@gmail.com",
-#         password="000111"
-#     )
-# except IntegrityError:
-#     print({'error': 'Este e-mail já está cadastrado.'})
+try:
+    user = Users.create(
+            name="Luis Souza",
+            email="souzaluis@hotmail.com",
+            password="000111"
+        )
+except IntegrityError:
+    print({'error': 'Este usuario já está cadastrado.'})
+
+
+try:
+    user = Users.create(
+            name="Agatha Dantas",
+            email="datas@outlook.com",
+            password="000111"
+        )
+except IntegrityError:
+    print({'error': 'Este usuario já está cadastrado.'})
+
+
+try:
+    user = Users.create(
+        name="ProgamadorPython",
+        email="devpython@gmail.com",
+        password="000111"
+    )
+except IntegrityError:
+    print({'error': 'Este e-mail já está cadastrado.'})
 
 
 # try:
@@ -42,6 +55,9 @@ db.create_tables([Users, Announcement])
 # except DoesNotExist:
 #     print('Usuario não existe.')
 
+
+
+
 luiz = Users.get(Users.email == 'souzaluis@hotmail.com')
 
 announ = Announcement.create(
@@ -50,5 +66,30 @@ announ = Announcement.create(
     description = "Tirar fotos nas entradas das lojas do terreo.",
     value = 600.00
 )
+announ = Announcement.create(
+    user = luiz,
+    title = "Video Maker: PizzaHut",
+    description = "Gravar o preparo da pizza.",
+    value = 1200.00
+)
+announ = Announcement.create(
+    user = luiz,
+    title = "Renderizar Videos: GameStation",
+    description = "Renderizar os videos da captação do shopping.",
+    value = 3200.00
+)
 
-print("Novo Anúncio: ", announ.id, announ.title)
+# user = Users.get(Users.email == 'dias@gmail.com')
+
+# print(f'ID: {user.id}, Nome: {user.name}, e-mail: {user.email}')
+
+# print('Usuário: ', user.id, user.name, user.email)
+
+# print("Novo Anúncio: ", announ.id, announ.title)
+
+print('Anuncíos Luiz:')
+announ_luiz = Announcement.select().join(Users).where(
+    Users.email == "souzaluis@hotmail.com"
+)
+for announ in announ_luiz:
+    print(f'Anun id: {announ.id}, Title Anun: {announ.title},Value: {announ.value}')
