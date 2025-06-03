0cc0c4a64ca9295ff4e58a6434224bf06ca5df98
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-03 15:57:07 -0300

    wip: todo

--- django_study/blog/settings.py
+++ django_study/blog/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#TODO: Get secret git from .env
 SECRET_KEY = 'django-insecure-%=$y(_@b!@l+($aa)-vd*)1)a+f-r8u#7r8wj@dpsuspvz457g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
