e1d170f0e556bd23620232df4490e5a907f94db9
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-09 14:41:59 -0300

    feat: base test announcement

--- django_study/announcements/models.py
+++ django_study/announcements/models.py
@@ -5,4 +5,21 @@ from django.utils import timezone
 # Create your models here.
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        
+        if self.pk:
+            self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
+    
+
+class Annoucement(BaseModel):
+    name = models.CharField(max_length=100)
+    
+
+    class Meta:
+        app_label = 'announcements' # 
