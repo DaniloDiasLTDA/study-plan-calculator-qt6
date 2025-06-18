020f91cd3749ea005b09d9c6249a556daae85c4d
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-18 17:16:56 -0300

    chore: order settings

--- django_blog/danilodias/settings.py
+++ django_blog/danilodias/settings.py
@@ -13,10 +13,10 @@ import os
 from pathlib import Path
 
 
+APP_NAME = 'danilodias'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-APP_NAME = 'danilodias'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
