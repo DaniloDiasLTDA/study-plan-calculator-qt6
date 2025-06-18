5e550ef100f0f51589b2d7f59648a75fd425bf1e
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-18 16:26:40 -0300

    feat: setup app name

--- django_blog/danilodias_dev/settings.py
+++ django_blog/danilodias_dev/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@--^j$fso41z4o2ulo!!gx$0bl48yra+j&ihk0_&pz9xrk7^qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0))) #Buscar por variaveis de ambiente
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APP_SETTINGS_PATH = 'danilodias_dev.settings'
--- django_blog/manage.py
+++ django_blog/manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+from danilodias_dev.settings import APP_SETTINGS_PATH 
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', APP_SETTINGS_PATH)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
