0a01b1ae9f75c5688d9699165b87fb11ec40321b
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-23 18:38:11 -0300

    wip: cleaning code

--- django_blog/danilodias/settings.py
+++ django_blog/danilodias/settings.py
@@ -18,14 +18,6 @@ DEBUG = os.environ.get('DEBUG', False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
-# ALLOWED_HOSTS = [
-#     h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
-#     if h.strip()
-# ]
-
-print(os.environ.get('ALLOWED_HOSTS'))
 ALLOWED_HOSTS = list(os.environ.get('ALLOWED_HOSTS', []))
 
 # Application definition
