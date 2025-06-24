0f93bfdd9d7909436a027809e2762df40024e5f3
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-23 23:30:42 -0300

    fix: fix --unsafe-fixes

--- django_blog/danilodias/asgi.py
+++ django_blog/danilodias/asgi.py
@@ -3,6 +3,8 @@ import os
 from danilodias.settings import APP_SETTINGS_PAT
 from django.core.asgi import get_asgi_application
 
+from danilodias.settings import APP_SETTINGS_PAT
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', APP_SETTINGS_PAT)
 
 application = get_asgi_application()
