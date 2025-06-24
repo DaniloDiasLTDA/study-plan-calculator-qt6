3d75ab979d75f7b7ee4f3260742ee8436ac6bae9
DaniloDiasLTDA <danilof.dias3@hotmail.com>
2025-06-23 21:05:56 -0300

    feat: test fix setup ci

--- django_blog/danilodias/settings.py
+++ django_blog/danilodias/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 import os
+
 from dotenv import load_dotenv
 from pathlib import Path
+
 
 load_dotenv()
 
